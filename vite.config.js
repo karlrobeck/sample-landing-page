2f4781be6c11cccc55b2c8b743ac0006904b3202
karl robeck alferez <karlalferezfx@gmail.com>
2023-07-14 01:40:44 +0800

    init git action

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base:'/sample-landing-page',
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+    base: '/',
+  }
+
+  if (command !== 'serve') {
+    config.base = '/sample-landing-page/'
+  }
+
+  return config
 })
